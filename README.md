95ebebe5a4b03da50ae3409475de6836bd302fe7
Paul Gadi <paulgadi@gmail.com>
2019-03-07 15:19:13 +0800

    initial one click deploy test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cimds
 One-click deploy via Heroku of a cryptoitem metadata server
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ server.js
@@ -0,0 +1,31 @@
+const Koa = require('koa');
+
+const mount = require('koa-mount');
+const graphqlHTTP = require('koa-graphql');
+const cors = require('@koa/cors');
+
+require('dotenv').config()
+
+
+const schema = require('./graphql/schema');
+
+const initDB = require('./database');
+
+initDB();
+
+const app = new Koa();
+
+app.listen(process.env.PORT || 9000);
+
+app.use(cors());
+
+app.use(mount('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+})))
+
+app.on('error', err => {
+  log.error('server error', err)
+});
+
+
