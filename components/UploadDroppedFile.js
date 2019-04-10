6f1902a7e006a3b3e974e8385d680149fc4c34a4
Paul Gadi <paulgadi@gmail.com>
2019-04-10 13:10:43 +0800

    initial refactor of item upload to staart providers

--- components/UploadDroppedFile.js
+++ components/UploadDroppedFile.js
@@ -1,7 +1,70 @@
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
+import { Component } from 'react'
+import { compose } from 'recompose';
+import { withUser } from 'ooth-client-react';
+
 import uploadsQuery from '../queries/uploads'
 import FileDropzone from './FileDropzone'
+
+const UploadFileQuery = gql`
+  mutation($files: [Upload!]!) {
+    multipleUpload(files: $files) {
+      _id
+      filename
+      contentType
+    }
+  }
+`
+
+class UploadDroppedFileComponent extends React.Component {
+  render() {
+    const { user, mutate, onUpload } = this.props;
+
+  return (
+    <div>
+
+      <FileDropzone type="file" multiple onChange={(files) => {
+        mutate({
+          variables: {
+            files: files
+          }
+        })
+        .then( ({data}) => {
+          if (onUpload) {
+            onUpload();
+          }
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+      }}/>
+
+    </div>
+    )
+  }
+}
+
+/*
+<form
+  onSubmit={(e) => {
+    e.preventDefault();
+    mutate({
+      variables: {
+        title: this.title.value,
+        content: this.content.value,
+      },
+    })
+      .then(({ data }) => {
+        if (onCreatePost) {
+          onCreatePost();
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }}
+>
 
 const UploadDroppedFile = ({ mutate }) => {
   const handleChange = (files) =>
@@ -15,23 +78,13 @@ const UploadDroppedFile = ({ mutate }) => {
       ) {
         const data = proxy.readQuery({ query: uploadsQuery })
         data.uploads.push(...multipleUpload)
+        console.log(multipleUpload)
+        console.log(proxy)
         proxy.writeQuery({ query: uploadsQuery, data })
       }
     })
 
-  return (
-    <div>
-      <FileDropzone type="file" multiple onChange={handleChange}/>
-    </div>
-  )
-}
+*/
 
-export default graphql(gql`
-  mutation($files: [Upload!]!) {
-    multipleUpload(files: $files) {
-      _id
-      filename
-      contentType
-    }
-  }
-`)(UploadDroppedFile)
+
+export default compose(withUser,graphql(UploadFileQuery))(UploadDroppedFileComponent)
--- pages/items.js
+++ pages/items.js
@@ -1,52 +1,12 @@
 import Layout from '../components/layout'
 import withPage from '../providers/page'
-
-import Page from '../components/Page'
-import Section from '../components/Section'
-import UploadBlob from '../components/UploadBlob'
-import UploadDroppedFile from '../components/UploadDroppedFile'
-import UploadFileList from '../components/UploadFileList'
-import Uploads from '../components/Uploads'
-import ItemDefinitions from '../components/ItemDefinitions'
-import AddItemDefinition from '../components/AddItemDefinition'
-import ItemInstances from '../components/ItemInstances'
-import ItemCollection from '../components/ItemCollection'
-import AddItemToCollection from '../components/AddItemToCollection'
-import Web3Initializer from '../components/Web3Initializer'
-
-import UploadFile from '../components/UploadFile'
+import ItemsPage from '../components/ItemsPage'
 
 export default withPage(() => (
 
   <Layout title="Items" page="items">
-  <Page>
-    {/*
-      <Web3Initializer/>
-    */}
     <div className="container">
-      <Section heading="Upload Image / 3D File">
-      {/*
-        <UploadDroppedFile />
-      */}
-        <UploadFile />
-      </Section>
-      <Section heading="Create Item Definition">
-        <AddItemDefinition />
-      </Section>
-      <Section heading="Add Item to Collection">
-        <AddItemToCollection />
-      </Section>
-      <Section heading="Uploads">
-        <Uploads />
-      </Section>
-      <Section heading="Item Definitions">
-        <ItemDefinitions />
-      </Section>
-      <Section heading="Item Collection">
-        <ItemInstances />
-      </Section>
+      <ItemsPage />
     </div>
-  </Page>
-</Layout>
-
+  </Layout>
 ))
