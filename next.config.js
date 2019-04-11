8cab178843ba2d769f139c337e8b9e160cfa0bc8
Paul Gadi <paulgadi@gmail.com>
2019-04-12 02:54:12 +0800

    attempt heroku deploy fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 // const withCSS = require('@zeit/next-css')
 // module.exports = withCSS()
 
-const BabiliPlugin = require('babili-webpack-plugin');
+//  const BabiliPlugin = require('babili-webpack-plugin');
+require('dotenv').config()
 
 module.exports = {
   publicRuntimeConfig: {
@@ -10,20 +11,20 @@ module.exports = {
     googleClientId: process.env.GOOGLE_CLIENT_ID,
     twitterClientId: process.env.TWITTER_CLIENT_ID,
   },
-  serverRuntimeConfig: {
-    url: process.env.SERVER_SIDE_URL || process.env.URL,
-  },
-  webpack(config, { dev }) {
-    // remove Uglify plugin
-    config.plugins = config.plugins.filter((plugin) => {
-      return plugin.constructor.name !== 'UglifyJsPlugin';
-    });
-
-    if (!dev) {
-      // add Babili plugin
-      config.plugins.push(new BabiliPlugin());
-    }
-
-    return config;
-  },
+  // serverRuntimeConfig: {
+  //   url: process.env.SERVER_SIDE_URL || process.env.URL,
+  // },
+  // webpack(config, { dev }) {
+  //   // remove Uglify plugin
+  //   config.plugins = config.plugins.filter((plugin) => {
+  //     return plugin.constructor.name !== 'UglifyJsPlugin';
+  //   });
+  //
+  //   if (!dev) {
+  //     // add Babili plugin
+  //     config.plugins.push(new BabiliPlugin());
+  //   }
+  //
+  //   return config;
+  // },
 };
